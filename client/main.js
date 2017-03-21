e184512a5692e7cc190774a1452587662f61173a
Ronny Kalesberg <Kalesberg@users.noreply.github.com>
2017-03-21 23:39:54 +0800

    Add files via upload
    client part

---
+++ client/main.js
@@ -0,0 +1,7 @@
+import angular from 'angular';
+import angularMeteor from 'angular-meteor';
+import 'bootstrap/dist/css/bootstrap.css';
+
+import { name as base } from '../imports/components/phoenix/phoenix';
+
+import 'bootstrap/dist/js/bootstrap.min';
