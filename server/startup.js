b4e9f61bdbe177b79e67ada3593b33d995ac758d
Ronny Kalesberg <Kalesberg@users.noreply.github.com>
2017-03-22 07:31:45 +0800

    server side startup - initial commit

---
+++ server/startup.js
@@ -0,0 +1,12 @@
+import { Broadcasts } from "../imports/api/broadcasts";
+import { Conversations } from "../imports/api/conversations";
+import { Groups } from "../imports/api/groups";
+import { Replies } from "../imports/api/replies";
+import { Features } from "../imports/api/features";
+import { Trackers } from "../imports/api/trackers";
+import { Images } from "../imports/api/images";
+import { Tags } from "../imports/api/tags";
+
+Meteor.startup(() => {
+  
+});
